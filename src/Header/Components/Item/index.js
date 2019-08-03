c8d5ce3bee45dbe3868cdcbe9161a0beb333cc5e
Klaudia <klaudia.krzywda@gmail.com>
2019-08-03 17:14:18 +0100

    Create Item component and render it in Header

---
+++ src/Header/Components/Item/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Item({ label, href }) {
+    return (
+        <a className="Item" href={href}>
+            {label}
+        </a>
+    )
+}
+export default Item;
+
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import Item from './Components/Item';
 
 function Header() {
+    const items = [
+        {
+            label: 'Home',
+            href: '#home',
+        },
+        {
+            label: 'About',
+            href: '#about',
+        },
+        {
+            label: 'Services',
+            href: '#services',
+        },
+        {
+            label: 'Gallery',
+            href: '#gallery',
+        },
+        {
+            label: 'Blog',
+            href: '#blog',
+        },
+        {
+            label: 'Contact',
+            href: '#contact',
+        },
+    ]
     return (
-        <div className="Header">Header</div>
+        <div className="Header">
+            <h4>Cahee</h4>
+            <nav>
+                {items.map(item => <Item label={item.label} href={item.href} />)}
+            </nav>
+        </div>
     )
 }
 export default Header;
