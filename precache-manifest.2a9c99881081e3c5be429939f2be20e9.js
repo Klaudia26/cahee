def8a64be5a639c0f547d4e41ac0b8369eccef3c
Klaudia <klaudia.krzywda@gmail.com>
2019-08-24 18:40:08 +0100

    Updates

---
+++ precache-manifest.2a9c99881081e3c5be429939f2be20e9.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f4428dbd2325a8031359e92629c8e8",
+    "url": "/cahee/index.html"
+  },
+  {
+    "revision": "c6e07088a69af1b7a6db",
+    "url": "/cahee/static/css/2.71946c96.chunk.css"
+  },
+  {
+    "revision": "e10d6e3e3c418604b887",
+    "url": "/cahee/static/css/main.37128c25.chunk.css"
+  },
+  {
+    "revision": "c6e07088a69af1b7a6db",
+    "url": "/cahee/static/js/2.108a629c.chunk.js"
+  },
+  {
+    "revision": "e10d6e3e3c418604b887",
+    "url": "/cahee/static/js/main.5ce9243c.chunk.js"
+  },
+  {
+    "revision": "5d8d0510f95e78be79ae",
+    "url": "/cahee/static/js/runtime~main.c9341574.js"
+  },
+  {
+    "revision": "9ed4ff540471f1c9d14198e1468dfc00",
+    "url": "/cahee/static/media/about-photo.9ed4ff54.png"
+  },
+  {
+    "revision": "fe452af1a54e8c58f6760f3489d8d0ee",
+    "url": "/cahee/static/media/blog-photo-1.fe452af1.jpg"
+  },
+  {
+    "revision": "daf3fbde5a2758fa429a7b470cff8870",
+    "url": "/cahee/static/media/blog-photo-2.daf3fbde.jpg"
+  },
+  {
+    "revision": "51ef0f68d5eea50a5a9ce7a84ede3f2a",
+    "url": "/cahee/static/media/blog-photo-3.51ef0f68.jpg"
+  },
+  {
+    "revision": "70e033c11c7482ff9bb5e538084d2833",
+    "url": "/cahee/static/media/blueberries.70e033c1.png"
+  },
+  {
+    "revision": "7e79745c117d46241de276d42c6d9fbe",
+    "url": "/cahee/static/media/contact-photo.7e79745c.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cahee/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cahee/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cahee/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cahee/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cahee/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0f543500027a45080e26abb185fd695b",
+    "url": "/cahee/static/media/home-photo.0f543500.png"
+  },
+  {
+    "revision": "ed9d52944ddc2b685481650aca4dffc3",
+    "url": "/cahee/static/media/photo-1.ed9d5294.jpg"
+  },
+  {
+    "revision": "d1ed8ed37812b884b70638024aa04363",
+    "url": "/cahee/static/media/photo-2.d1ed8ed3.jpg"
+  },
+  {
+    "revision": "39ae74b183f6fe4082bae4cef0b8fa38",
+    "url": "/cahee/static/media/photo-3.39ae74b1.jpg"
+  },
+  {
+    "revision": "1534fe74dd82306cb54bc332778bd26d",
+    "url": "/cahee/static/media/photo-4.1534fe74.jpg"
+  },
+  {
+    "revision": "e178e356dab7760fabbff58a535a688b",
+    "url": "/cahee/static/media/photo-5.e178e356.jpg"
+  },
+  {
+    "revision": "1dbfad545b05aa947c97c3fe5464b890",
+    "url": "/cahee/static/media/purepng.1dbfad54.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1016d56b6be8e946d3ded8a7821da91c.js"
+  "/cahee/precache-manifest.2a9c99881081e3c5be429939f2be20e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cahee/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
