915ab247c2ffe6f7998acd8fbd7add047e22c19c
Klaudia <klaudia.krzywda@gmail.com>
2019-08-26 12:08:42 +0100

    section contact style

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -21,22 +21,16 @@ class Contact extends Component {
                 <h2 className='heading-primary'>Contact</h2>
                 <p className='paragraph'>For new projectsideas business or anything you'd like to discuss with me!</p>
                 <div className='form-box'>
-                    <form>
-                        <input name="name" type="text" value={this.state.name} placeholder='Name' onChange={this.handleChange} />
-                        <input name="email" type="email" value={this.state.email} placeholder='Your Emial...' onChange={this.handleChange} />
-                        <input name="phone" type="tel" value={this.state.phone} placeholder='Your Phone Number...' onChange={this.handleChange} />
-                        <textarea name="message" value={this.state.message} placeholder='Your Message...' onChange={this.handleChange} />
+                    <form className="contact-form">
+                        <input className="contact-input" name="name" type="text" value={this.state.name} placeholder='Name' onChange={this.handleChange} />
+                        <input className="contact-input" name="email" type="email" value={this.state.email} placeholder='Your Emial...' onChange={this.handleChange} />
+                        <input className="contact-input" name="phone" type="tel" value={this.state.phone} placeholder='Your Phone Number...' onChange={this.handleChange} />
+                        <textarea className="contact-input" name="message" value={this.state.message} placeholder='Your Message...' onChange={this.handleChange} />
                     </form>
-                    <address>
-                        Company:
-                         <br />
-                        Cahee
-                        <br />
-                        Address:
-                        <br />
-                        Street 90, NN-9999 London
-                        <br />
-                        UK
+                    <address className="adress-contact">
+                        Company: Cahee
+                        <br></br>
+                        Address: Street 90, NN-9999 London UK
                     </address>
                 </div>
             </section>
